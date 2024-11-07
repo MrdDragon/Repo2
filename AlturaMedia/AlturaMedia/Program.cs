2c492dfce3eb36d68381ab6533dc3d04d4e8a188
Daniel <danielcoliveira9@gmail.com>
2024-11-07 12:09:16 +0000

    Update Program.cs

--- AlturaMedia/AlturaMedia/Program.cs
+++ AlturaMedia/AlturaMedia/Program.cs
@@ -7,7 +7,17 @@ namespace AlturaMedia
     {
         static void Main(string[] args)
         {
+            int nPessoas = 0;
+            Console.WriteLine("Quantas alturas de pessoas vai introduzir: ");
+            nPessoas = int.Parse(Console.ReadLine());
 
+            double[] alturas = new double[nPessoas];
+
+            for(int i = 0; i < nPessoas; i++)
+            {
+                Console.WriteLine("Introduza a altura da " + (i + 1) + "ª pessoa: ");
+                alturas[i] = double.Parse(Console.ReadLine());
+            }
         }
     }
 }
