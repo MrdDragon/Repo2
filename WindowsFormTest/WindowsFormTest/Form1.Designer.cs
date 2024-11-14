c7ca62d2ee073918207ea0725dbd9c3467adf3ed
Daniel <danielcoliveira9@gmail.com>
2024-11-14 00:14:29 +0000

    WFtest

--- WindowsFormTest/WindowsFormTest/Form1.Designer.cs
+++ WindowsFormTest/WindowsFormTest/Form1.Designer.cs
@@ -28,69 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_Nome = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Cidade = new System.Windows.Forms.TextBox();
+            this.txt_label1 = new System.Windows.Forms.Label();
+            this.txt_label = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bt_Inserir = new System.Windows.Forms.Button();
+            this.bt_Atualizar = new System.Windows.Forms.Button();
+            this.bt_Apagar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txt_Nome
             // 
-            this.textBox1.Location = new System.Drawing.Point(253, 154);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(194, 22);
-            this.textBox1.TabIndex = 0;
+            this.txt_Nome.Location = new System.Drawing.Point(12, 59);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(194, 22);
+            this.txt_Nome.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(255, 62);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 25);
+            this.label1.Size = new System.Drawing.Size(87, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "LOGIN";
+            this.label1.Text = "Registos";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox2
+            // txt_Cidade
             // 
-            this.textBox2.Location = new System.Drawing.Point(253, 236);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(194, 22);
-            this.textBox2.TabIndex = 2;
+            this.txt_Cidade.Location = new System.Drawing.Point(12, 113);
+            this.txt_Cidade.Name = "txt_Cidade";
+            this.txt_Cidade.Size = new System.Drawing.Size(194, 22);
+            this.txt_Cidade.TabIndex = 2;
             // 
-            // label2
+            // txt_label1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(257, 217);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 16);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Senha";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.txt_label1.AutoSize = true;
+            this.txt_label1.Location = new System.Drawing.Point(9, 94);
+            this.txt_label1.Name = "txt_label1";
+            this.txt_label1.Size = new System.Drawing.Size(51, 16);
+            this.txt_label1.TabIndex = 3;
+            this.txt_label1.Text = "Cidade";
+            this.txt_label1.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label3
+            // txt_label
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(257, 135);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 16);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Username";
+            this.txt_label.AutoSize = true;
+            this.txt_label.Location = new System.Drawing.Point(9, 40);
+            this.txt_label.Name = "txt_label";
+            this.txt_label.Size = new System.Drawing.Size(44, 16);
+            this.txt_label.TabIndex = 4;
+            this.txt_label.Text = "Nome";
+            this.txt_label.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(712, 386);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // bt_Inserir
+            // 
+            this.bt_Inserir.Location = new System.Drawing.Point(259, 94);
+            this.bt_Inserir.Name = "bt_Inserir";
+            this.bt_Inserir.Size = new System.Drawing.Size(88, 41);
+            this.bt_Inserir.TabIndex = 6;
+            this.bt_Inserir.Text = "Inserir";
+            this.bt_Inserir.UseVisualStyleBackColor = true;
+            this.bt_Inserir.Click += new System.EventHandler(this.bt_Inserir_Click);
+            // 
+            // bt_Atualizar
+            // 
+            this.bt_Atualizar.Location = new System.Drawing.Point(353, 94);
+            this.bt_Atualizar.Name = "bt_Atualizar";
+            this.bt_Atualizar.Size = new System.Drawing.Size(88, 41);
+            this.bt_Atualizar.TabIndex = 7;
+            this.bt_Atualizar.Text = "Atualizar";
+            this.bt_Atualizar.UseVisualStyleBackColor = true;
+            this.bt_Atualizar.Click += new System.EventHandler(this.bt_Atualizar_Click);
+            // 
+            // bt_Apagar
+            // 
+            this.bt_Apagar.Location = new System.Drawing.Point(447, 94);
+            this.bt_Apagar.Name = "bt_Apagar";
+            this.bt_Apagar.Size = new System.Drawing.Size(88, 41);
+            this.bt_Apagar.TabIndex = 8;
+            this.bt_Apagar.Text = "Apagar";
+            this.bt_Apagar.UseVisualStyleBackColor = true;
+            this.bt_Apagar.Click += new System.EventHandler(this.bt_Apagar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(702, 384);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(736, 563);
+            this.Controls.Add(this.bt_Apagar);
+            this.Controls.Add(this.bt_Atualizar);
+            this.Controls.Add(this.bt_Inserir);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_label);
+            this.Controls.Add(this.txt_label1);
+            this.Controls.Add(this.txt_Cidade);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_Nome);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,11 +149,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_Nome;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Cidade;
+        private System.Windows.Forms.Label txt_label1;
+        private System.Windows.Forms.Label txt_label;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bt_Inserir;
+        private System.Windows.Forms.Button bt_Atualizar;
+        private System.Windows.Forms.Button bt_Apagar;
     }
 }
 
--- WindowsFormTest/WindowsFormTest/Form1.cs
+++ WindowsFormTest/WindowsFormTest/Form1.cs
@@ -1,20 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormTest
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Data Source=MSI\\SQLEXPRESS;Initial Catalog=demodb;Integrated Security=true;");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+
+        int ID = 0; //Variavel para guardar registo selecionado
         public Form1()
         {
             InitializeComponent();
+            DisplayData();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -25,6 +26,98 @@ namespace WindowsFormTest
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_Apagar_Click(object sender, EventArgs e)
+        {
+            if (ID != 0)
+            {
+                cmd = new SqlCommand("delete tbl_NCidade where ID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Registo apagado com sucesso!");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Porfavor selecione registo para apagar");
+            }
+        }
+
+        private void bt_Inserir_Click(object sender, EventArgs e)
+        {
+            if(txt_Nome.Text != "" && txt_Cidade.Text != "")
+            {
+                cmd = new SqlCommand("update tbl_NCidade set Nome=@nome,Cidade=@cidade where ID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.Parameters.AddWithValue("@nome", txt_Nome.Text);
+                cmd.Parameters.AddWithValue("@cidade", txt_Cidade.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Dados Atualizados com Sucesso");
+                con.Close();
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Preencha todos os campos!");
+            }
+        }
+
+        //Mostrar dados no DataGridView
+        private void DisplayData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("select * from tbl_NCidade", con);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        //Limpar Dados
+        private void ClearData()
+        {
+            txt_Nome.Text = "";
+            txt_Cidade.Text = "";
+            ID = 0;
+        }
+
+        //dataGridView1 RowHeaderMouseClick Event
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txt_Nome.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txt_Cidade.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void bt_Atualizar_Click(object sender, EventArgs e)
+        {
+            if (txt_Nome.Text != "" && txt_Cidade.Text != "")
+            {
+                cmd = new SqlCommand("update tbl_NCidade set Nome=@nome,Cidade=@cidade where ID=@id", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.Parameters.AddWithValue("@nome", txt_Nome.Text);
+                cmd.Parameters.AddWithValue("@cidade", txt_Cidade.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Dados Atualizados com Sucesso");
+                con.Close();
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Porfavor Selectiona Registo para Atualizar");
+            }
         }
     }
 }
