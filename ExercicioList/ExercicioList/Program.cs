b3cec88358c21c1a13410b87a49ffdad860b9d5e
Daniel <danielcoliveira9@gmail.com>
2024-12-08 21:33:53 +0000

    List<T>

---
+++ ExercicioList/ExercicioList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> listaNomes = new List<string> { "Alice", "Joana", "Jose" };
+            listaNomes.Add("Daniel");
+
+            Console.WriteLine("List<T>:");
+
+            foreach (string nome in listaNomes)
+            {
+                Console.WriteLine("\t"+nome);
+            }
+            Console.ReadKey();
+        }
+    }
+}
