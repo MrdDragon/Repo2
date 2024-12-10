4d9f9270dc40c830f22505713c58162f686bab51
Daniel <danielcoliveira9@gmail.com>
2024-12-10 11:34:24 +0000

    Update Program.cs

--- EparMetodo/EparMetodo/Program.cs
+++ EparMetodo/EparMetodo/Program.cs
@@ -8,16 +8,49 @@ namespace EparMetodo
 {
     internal class Program
     {
-        static bool Epar(int num)
+        static void Epar(int num)
         {
             if (num % 2 == 0) 
             {
-                return true;
+                Console.WriteLine("O número é par!");
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar!");
             }
         }
+
+        static bool Eprimo(int numP)
+        {
+            if (numP <= 1)
+                return false;
+            for (int i =2; i <= Math.Sqrt(numP); i++)
+            {
+                if(numP % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("\nIntroduza um número: ");
+            int numero = int.Parse(Console.ReadLine());
+            Epar(numero);
 
+            if (Eprimo(numero))
+            {
+                Console.WriteLine("O número é Primo!");
+            }
+            else 
+            {
+                Console.WriteLine("O número não é Primo!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
