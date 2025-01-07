dcf05a23d496d68a146f1c732df0ab1f208337c4
Daniel <danielcoliveira9@gmail.com>
2025-01-07 02:41:18 +0000

    Create ClassTriangulo.cs

---
+++ AreaTriangulo/AreaTriangulo/ClassTriangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangulo
+{ 
+    public class ClassTriangulo
+    {
+        public double AreaTriangulo(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        public static double AreaTrianguloStatic(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
